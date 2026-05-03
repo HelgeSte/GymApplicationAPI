8000f4c5a0780d11fe4d8267ca527e9d2b164741
Helge Stegemoen <helge@stegemoen.com>
2026-05-03 21:46:14 +0200

    Session controller and dto

---
+++ GymApplicationAPI/GymApplicationAPI/DTOs/SessionDTOs.cs
@@ -0,0 +1,33 @@
+﻿namespace GymApplicationAPI.DTOs;
+
+public class SessionDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public int TotalSpots { get; set; }
+    public int AvailableSpots { get; set; }
+    public string Color { get; set; } = string.Empty;
+}
+
+public class CreateSessionRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public int TotalSpots { get; set; }
+    public string Color { get; set; } = "bg-blue-200";
+}
+
+public class UpdateSessionRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public int TotalSpots { get; set; }
+    public string Color { get; set; } = string.Empty;
+}
